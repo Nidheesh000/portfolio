e0a03e3b0918a21fb12df885abb8005d2e7b1dd1
bad974415boy-sys <bad974415boy@gmail.com>
2026-06-06 11:57:53 +0530

    first commit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
                 }}
               />
               <img
-                src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=500&auto=format&fit=crop&q=80"
+                src="c:\Users\NETCOM\Pictures\Screenshots\Screenshot 2026-05-16 142050.png"
                 alt="Profile Avatar"
                 className="position-relative img-fluid rounded-4 shadow-lg"
                 style={{
